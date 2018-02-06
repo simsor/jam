d7e2984f099c0c328c44ca69b38b28f6e1a6ddcf
Simon Garrelou <simon.garrelou@gmail.com>
2018-02-06 17:23:22 +0100

    Start working on windows version

--- beep_windows.go
+++ beep_windows.go
@@ -5,25 +5,25 @@ import (
 )
 
 type Beeper struct {
-	kernel32 uintptr
-	procBeep uintptr
+	kernel32 *syscall.DLL
+	procBeep *syscall.Proc
 }
 
 func NewBeeper() (*Beeper, error) {
-	b := make(Beeper)
+	b := &Beeper{}
 
-	h := syscall.MustLoadLibrary("kernel32.dll")
+	h := syscall.MustLoadDLL("kernel32.dll")
 	b.kernel32 = h
 
-	h := syscall.MustGetProcAddress(b.kernel32, "Beep")
-	b.procBeep = h
+	proc := b.kernel32.MustFindProc("Beep")
+	b.procBeep = proc
 
 	return b, nil
 }
 
 func (b *Beeper) Beep(freq float32, dur int) {
-	syscall.Syscall(b.procBeep, 2,
-		uintptr(int(freq)),
-		uintptr(dur),
-		0)
+	r1, _, lastErr := b.procBeep.Call(uintptr(freq), uintptr(dur))
+	if r1 != 0 {
+		panic(lastErr)
+	}
 }
