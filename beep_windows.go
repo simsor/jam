8d5df82cd4dadb0e806c01e87352f18fc1fac592
Simon Garrelou <simon.garrelou@riseup.net>
2018-02-07 13:35:01 +0100

    Refactor the actual beeping functionnality in jammer.go

--- beep_windows.go
+++ beep_windows.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"syscall"
-)
-
 type Beeper struct {
-	kernel32 *syscall.DLL
-	procBeep *syscall.Proc
 }
 
 func NewBeeper() (*Beeper, error) {
-	b := &Beeper{}
-
-	h := syscall.MustLoadDLL("kernel32.dll")
-	b.kernel32 = h
-
-	proc := b.kernel32.MustFindProc("Beep")
-	b.procBeep = proc
-
-	return b, nil
+	panic("Sorry, but beep-jam doesn't work on Windows :(\nUse the Web version!")
 }
 
 func (b *Beeper) Beep(freq float32, dur int) {
-	r1, _, lastErr := b.procBeep.Call(uintptr(freq), uintptr(dur))
-	if r1 != 0 {
-		panic(lastErr)
-	}
 }
